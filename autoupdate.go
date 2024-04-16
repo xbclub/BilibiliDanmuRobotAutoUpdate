432e2fd9ff90e7a2d1684a88eb02db69a26375ff
yii <yii@42cloud.cn>
2024-04-16 09:18:46 +0800

    更新 go-zero 依赖

--- autoupdate.go
+++ autoupdate.go
@@ -6,12 +6,9 @@ import (
 	"github.com/xbclub/BilibiliDanmuRobotAutoUpdate.git/internal/config"
 	"github.com/xbclub/BilibiliDanmuRobotAutoUpdate.git/internal/handler"
 	"github.com/xbclub/BilibiliDanmuRobotAutoUpdate.git/internal/svc"
-	"github.com/zeromicro/go-zero/core/logx"
-	"net/http"
-	"strings"
-
 	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/rest"
+	"net/http"
 )
 
 var configFile = flag.String("f", "etc/autoupdate.yaml", "the config file")
@@ -40,26 +37,27 @@ func main() {
 		panic(err)
 	}
 	handler.RegisterHandlers(server, ctx)
-	RegisterHandlers(server)
+	//RegisterHandlers(server)
 	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
 	server.Start()
 }
-func RegisterHandlers(engine *rest.Server) {
 
-	//这里注册
-	dirlevel := []string{":1", ":2", ":3", ":4", ":5", ":6", ":7", ":8"}
-	patern := "/test/"
-	dirpath := "./test/"
-	for i := 1; i < len(dirlevel); i++ {
-		path := patern + strings.Join(dirlevel[:i], "/")
-		//最后生成 /asset
-		engine.AddRoute(
-			rest.Route{
-				Method:  http.MethodGet,
-				Path:    path,
-				Handler: http.StripPrefix(patern, http.FileServer(http.Dir(dirpath))).ServeHTTP,
-			})
-
-		logx.Infof("register dir  %s  %s", path, dirpath)
-	}
-}
+//func RegisterHandlers(engine *rest.Server) {
+//
+//	//这里注册
+//	dirlevel := []string{":1", ":2", ":3", ":4", ":5", ":6", ":7", ":8"}
+//	patern := "/test/"
+//	dirpath := "./test/"
+//	for i := 1; i < len(dirlevel); i++ {
+//		path := patern + strings.Join(dirlevel[:i], "/")
+//		//最后生成 /asset
+//		engine.AddRoute(
+//			rest.Route{
+//				Method:  http.MethodGet,
+//				Path:    path,
+//				Handler: http.StripPrefix(patern, http.FileServer(http.Dir(dirpath))).ServeHTTP,
+//			})
+//
+//		logx.Infof("register dir  %s  %s", path, dirpath)
+//	}
+//}
